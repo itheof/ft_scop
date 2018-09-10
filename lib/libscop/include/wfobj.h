5a517794d9a211b586028d1246fa85fea2046294
Theophile VALLEE <tvallee@student.42.fr>
2018-09-10 13:46:41 +0200

    add obj parser

--- lib/libscop/include/wfobj.h
+++ lib/libscop/include/wfobj.h
@@ -6,38 +6,60 @@
 /*   By: tvallee <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/07 11:07:38 by tvallee           #+#    #+#             */
-/*   Updated: 2018/09/07 12:16:01 by tvallee          ###   ########.fr       */
+/*   Updated: 2018/09/10 12:49:19 by tvallee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WFOBJ_H
 # define WFOBJ_H
 
-#include "libft.h"
+# include "libft.h"
+
+# define MAX_POLYGON_SIDES 4
+
+# define COORD_X 0
+# define COORD_Y 1
+# define COORD_Z 2
+# define COORD_W 3
+
+# define TRIPLET_V 0
+# define TRIPLET_VT 1
+# define TRIPLET_VN 2
+
+typedef struct	s_vertex
+{
+	float	coord[4];
+	int		ndim;
+}				t_vertex;
+
+typedef struct	s_vertex_normal
+{
+	float	coord[3];
+}				t_vertex_normal;
+
+typedef struct	s_vertex_texture
+{
+	float	coord[3];
+	int		ndim;
+}				t_vertex_texture;
+
+typedef struct	s_face
+{
+	int		triplets[MAX_POLYGON_SIDES][3];
+	int		n_sides;
+	t_bool	normal;
+	t_bool	texture;
+}				t_face;
 
 typedef struct	s_wfobj
 {
 	t_list	*v;
+	size_t	n_v;
 	t_list	*vt;
+	size_t	n_vt;
 	t_list	*vn;
-	t_list	*vp;
-/*	t_list	*cstype;
-	t_list	*deg;
-	t_list	*bmat;
-	t_list	*step; */
-	t_list	*p;
-	t_list	*l;
+	size_t	n_vn;
 	t_list	*f;
-	t_list	*curv;
-	t_list	*curv2;
-	t_list	*surf;
-/*	t_list	*parm;
-	t_list	*trim;
-	t_list	*hole;
-	t_list	*scrv;
-	t_list	*sp;
-	t_list	*end;
-	t_list	*con;*/
 }				t_wfobj;
 
 t_wfobj	*wfobj_parse(FILE *stream);
--- lib/libscop/src/wfobj.c
+++ lib/libscop/src/wfobj.c
@@ -6,7 +6,7 @@
 /*   By: tvallee <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/07 10:41:37 by tvallee           #+#    #+#             */
-/*   Updated: 2018/09/07 12:33:44 by tvallee          ###   ########.fr       */
+/*   Updated: 2018/09/10 13:42:06 by tvallee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,67 +35,205 @@ static t_bool	unimplemented(t_wfobj *dst, char const *line, char const *kw)
 
 	(void)dst;
 	(void)line;
-	fprintf(stderr, "keyword `%s'unimplemented yet", kw);
+	fprintf(stderr, "keyword %sunimplemented", kw);
 	return (false);
 }
 
-/*
-static t_bool	parse_line(char const *line, t_list ** vertices_h, t_list **faces_h)
+static t_bool	parse_vertex(t_wfobj *dst, char const *line, char const *kw)
 {
-	float	vbuf[3];
-	int		fbuf[5];
-	t_list	*lst;
+	t_vertex	vertex;
+	t_list		*lst;
 
-	if (!line)
-	{
-		*vertices_h = NULL;
-		*faces_h = NULL;
-		return (true);
-	}
-
-	if (!strncmp(line, "v ", 2))
-	{
-		if (sscanf(line + 2, "%f %f %f", vbuf, vbuf + 1, vbuf + 2) != 3)
-			fprintf(stderr, "unsufficient vertex input\n");
-		else
-		{
-			if (!(lst = ft_lstnew(vbuf, sizeof(vbuf))))
-			{
-				perror("parse line malloc: ");
-				return (false);
-			}
-			ft_lstpushback(vertices_h, lst);
-		}
-	}
-
-	else if (!strncmp(line, "f ", 2))
-	{
-		if ((fbuf[0] = sscanf(line + 2, "%d %d %d %d", fbuf + 1, fbuf + 2, fbuf + 3, fbuf + 4)) < 3)
-			fprintf(stderr, "unsufficient face input\n");
-		else
-		{
-			if (!(lst = ft_lstnew(fbuf, sizeof(fbuf))))
-			{
-				perror("parse line malloc: ");
-				return (false);
-			}
-			ft_lstpushback(faces_h, lst);
-		}
-	}
-
-	else if (line[0] == '#')
-		;
+	(void)kw;
+	vertex.ndim = sscanf(line, "%f %f %f %f", vertex.coord + COORD_X,
+			vertex.coord + COORD_Y, vertex.coord + COORD_Z, vertex.coord + COORD_W);
+	if (vertex.ndim < 3)
+		fprintf(stderr, "unsufficient vertex input");
 	else
-		fprintf(stderr, "warning: ignored line: %s\n", line);
+	{
+		if (vertex.ndim != 4)
+			vertex.coord[COORD_W] = 1.0f;
+		if (!(lst = ft_lstnew(&vertex, sizeof(vertex))))
+		{
+			perror("parse_vertex malloc");
+			return (false);
+		}
+		ft_lstpushback(&dst->v, lst);
+		dst->n_v++;
+	}
 	return (true);
 }
-*/
+
+static t_bool	parse_vertex_normal(t_wfobj *dst, char const *line,
+		char const *kw)
+{
+	t_vertex_normal	vn;
+	t_list			*lst;
+	int				nscan;
+
+	(void)kw;
+	nscan = sscanf(line, "%f %f %f", vn.coord + COORD_X, vn.coord + COORD_Y,
+			vn.coord + COORD_Z);
+	if (nscan != 3)
+	{
+		fprintf(stderr, "unsufficient vertex normal input");
+		return (false);
+	}
+	if (!(lst = ft_lstnew(&vn, sizeof(vn))))
+	{
+		perror("parse_vertex_normal malloc");
+		return (false);
+	}
+	ft_lstpushback(&dst->vn, lst);
+	dst->n_vn++;
+	return (true);
+}
+
+static t_bool	parse_vertex_texture(t_wfobj *dst, char const *line,
+		char const *kw)
+{
+	t_vertex_texture	vt;
+	t_list				*lst;
+
+	(void)kw;
+	vt.ndim = sscanf(line, "%f %f %f", vt.coord + COORD_X, vt.coord + COORD_Y,
+			vt.coord + COORD_Z);
+	if (vt.ndim == 0)
+	{
+		fprintf(stderr, "unsufficient vertex texture input");
+		return (false);
+	}
+	if (vt.ndim < 3)
+		vt.coord[COORD_Z] = 0.0f;
+	if (vt.ndim < 2)
+		vt.coord[COORD_Y] = 0.0f;
+	if (!(lst = ft_lstnew(&vt, sizeof(vt))))
+	{
+		perror("parse_vertex_texture malloc");
+		return (false);
+	}
+	(void)kw;
+	ft_lstpushback(&dst->vt, lst);
+	dst->n_vt++;
+	return (true);
+}
+
+static t_bool	parse_face_push(t_wfobj *dst, t_face f)
+{
+	t_list	*lst;
+
+	if (!(lst = ft_lstnew(&f, sizeof(f))))
+	{
+		perror("parse_face_push malloc");
+		return (false);
+	}
+	ft_lstpushback(&dst->f, lst);
+	return (true);
+}
+
+/* vertices are 1-indexed /!\ */
+
+static t_bool	parse_face(t_wfobj *dst, char const *line, char const *kw)
+{
+	t_face	f;
+
+	(void)kw;
+	if ((f.n_sides = sscanf(line, "%d %d %d %d", &f.triplets[0][TRIPLET_V],
+					&f.triplets[1][TRIPLET_V], &f.triplets[2][TRIPLET_V],
+					&f.triplets[3][TRIPLET_V])) >= 3)
+	{
+		f.normal = false;
+		f.texture = false;
+		return (parse_face_push(dst, f));
+	}
+	else if ((f.n_sides = sscanf(line, "%d// %d// %d// %d//",
+				&f.triplets[0][TRIPLET_V], &f.triplets[1][TRIPLET_V],
+				&f.triplets[2][TRIPLET_V], &f.triplets[3][TRIPLET_V])) >= 3)
+	{
+		f.normal = false;
+		f.texture = false;
+		return (parse_face_push(dst, f));
+	}
+	else if ((f.n_sides = sscanf(line, "%d/%d/ %d/%d/ %d/%d/ %d/%d/",
+				&f.triplets[0][TRIPLET_V], &f.triplets[0][TRIPLET_VT],
+				&f.triplets[1][TRIPLET_V], &f.triplets[1][TRIPLET_VT],
+				&f.triplets[2][TRIPLET_V], &f.triplets[2][TRIPLET_VT],
+				&f.triplets[3][TRIPLET_V], &f.triplets[3][TRIPLET_VT]
+				)) >= 6)
+	{
+		f.texture = true;
+		f.normal = false;
+		return (parse_face_push(dst, f));
+	}
+	else if ((f.n_sides = sscanf(line, "%d//%d %d//%d %d//%d %d//%d",
+				&f.triplets[0][TRIPLET_V], &f.triplets[0][TRIPLET_VN],
+				&f.triplets[1][TRIPLET_V], &f.triplets[1][TRIPLET_VN],
+				&f.triplets[2][TRIPLET_V], &f.triplets[2][TRIPLET_VN],
+				&f.triplets[3][TRIPLET_V], &f.triplets[3][TRIPLET_VN]
+				)) >= 6)
+	{
+		f.texture = false;
+		f.normal = true;
+		return (parse_face_push(dst, f));
+	}
+	else if ((f.n_sides = sscanf(line, "%d/%d/%d %d/%d/%d %d/%d/%d %d/%d/%d/",
+				&f.triplets[0][TRIPLET_V], &f.triplets[0][TRIPLET_VT], &f.triplets[0][TRIPLET_VN],
+				&f.triplets[1][TRIPLET_V], &f.triplets[1][TRIPLET_VT], &f.triplets[1][TRIPLET_VN],
+				&f.triplets[2][TRIPLET_V], &f.triplets[2][TRIPLET_VT], &f.triplets[2][TRIPLET_VN],
+				&f.triplets[3][TRIPLET_V], &f.triplets[3][TRIPLET_VT], &f.triplets[3][TRIPLET_VN]
+				)) >= 9)
+	{
+		f.texture = true;
+		f.normal = true;
+		return (parse_face_push(dst, f));
+	}
+	else
+	{
+		fprintf(stderr, "f: illegal statement");
+		return (false);
+	}
+}
 
 #define N_KEYWORD (sizeof(g_kw_handler_lookup) / sizeof(*g_kw_handler_lookup))
 struct s_kw_handler	g_kw_handler_lookup[] = {
 	{"#", &comment_skip},
-	{"v ", &unimplemented},
-	{"f ", &unimplemented}
+	{"v ", &parse_vertex},
+	{"vn ", &parse_vertex_normal},
+	{"vt ", &parse_vertex_texture},
+	{"f ", &parse_face},
+
+	{"p ", &unimplemented},
+	{"l ", &unimplemented},
+
+	{"vp ", &unimplemented},
+	{"cstype ", &unimplemented},
+	{"deg ", &unimplemented},
+	{"bmat ", &unimplemented},
+	{"step ", &unimplemented},
+	{"curv ", &unimplemented},
+	{"curv2 ", &unimplemented},
+	{"surf ", &unimplemented},
+	{"parm ", &unimplemented},
+	{"trim ", &unimplemented},
+	{"hole ", &unimplemented},
+	{"scrv ", &unimplemented},
+	{"sp ", &unimplemented},
+	{"end ", &unimplemented},
+	{"con ", &unimplemented},
+	{"g ", &unimplemented},
+	{"s ", &unimplemented},
+	{"mg ", &unimplemented},
+	{"o ", &unimplemented},
+	{"bevel ", &unimplemented},
+	{"c_interp ", &unimplemented},
+	{"d_interp ", &unimplemented},
+	{"lod ", &unimplemented},
+	{"usemtl ", &unimplemented},
+	{"mtllib ", &unimplemented},
+	{"shadow_obj ", &unimplemented},
+	{"trace_obj ", &unimplemented},
+	{"ctech ", &unimplemented},
+	{"stech ", &unimplemented},
 };
 
 static inline t_bool	is_prefix(char const *pre, char const *str)
@@ -103,9 +241,10 @@ static inline t_bool	is_prefix(char const *pre, char const *str)
 	return (strncmp(pre, str, strlen(pre)) == 0);
 }
 
-static t_bool	parse_line(t_wfobj *dst, char const *line, size_t linen)
+static void	parse_line(t_wfobj *dst, char const *line, size_t linen)
 {
-	size_t	i;
+	struct s_kw_handler	*match;
+	size_t				i;
 
 	i = 0;
 	while (i < N_KEYWORD && !is_prefix(g_kw_handler_lookup[i].kw, line))
@@ -117,10 +256,14 @@ static t_bool	parse_line(t_wfobj *dst, char const *line, size_t linen)
 			i++;
 		fprintf(stderr, "unrecognized keyword `%.*s'", (int)i, line);
 	}
-	else if (g_kw_handler_lookup[i].handle(dst, line, g_kw_handler_lookup[i].kw))
-		return (true);
-	fprintf(stderr, ", ignoring line %zu...\n", linen);
-	return (true);
+	else
+	{
+		match = g_kw_handler_lookup + i;
+		if (match->handle(dst, line + strlen(match->kw), match->kw))
+			return ;
+	}
+	fprintf(stderr, ", skipping line %zu...\n", linen);
+	return ;
 }
 
 t_wfobj	*wfobj_parse(FILE *stream)
@@ -140,8 +283,7 @@ t_wfobj	*wfobj_parse(FILE *stream)
 
 		while (getline(&line, &size, stream) >= 0)
 		{
-			if (!parse_line(ret, line, linen))
-				break;
+			parse_line(ret, line, linen);
 			linen++;
 		}
 		free(line);
@@ -153,5 +295,27 @@ t_wfobj	*wfobj_parse(FILE *stream)
 
 void	wfobj_delete(t_wfobj *obj)
 {
-	(void)obj;
+	t_list	*(lists[4]);
+	t_list	*current;
+	t_list	*tmp;
+	size_t	i;
+
+	lists[0] = obj->v;
+	lists[1] = obj->vt;
+	lists[2] = obj->vn;
+	lists[3] = obj->f;
+	i = 0;
+	while (i < sizeof(lists) / sizeof(*lists))
+	{
+		current = lists[i];
+		while (current != NULL)
+		{
+			free(current->content);
+			tmp = current;
+			current = current->next;
+			free(tmp);
+		}
+		i++;
+	}
+	free(obj);
 }
